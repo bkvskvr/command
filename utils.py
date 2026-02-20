2f6b7855f16a836efff125d1bdeece369bc42d9e
darkovalenko <d.kovalenko1104@gmail.com>
2026-02-20 10:24:57 +0100

    Update2 utils.py

--- utils.py
+++ utils.py
@@ -17,3 +17,8 @@ def fibonacci(n):
     for _ in range(2, n + 1):
         a, b = b, a + b
     return b
+def is_prime(n):
+    if n <= 1: return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0: return False
+    return True
