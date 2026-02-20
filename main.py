c804c97b4aa2cf918869ebb56a4ce73247c5a3c9
katya15110 <vovchenko151107@gmail.com>
2026-02-20 12:07:05 +0200

    com

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ if name == "__main__":
     print("Факторіал числа 5:", utils.factorial(5))
 
 
-def gcd(a, b):
-    while b:
-        a, b = b, a % b
-    return a
+def gcd(b, a):
+    while a:
+        b, a = a, b % a
+    return b
